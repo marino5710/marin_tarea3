0827323bc61e1845e960109fd6eb54c13e3cf201
marino5710 <brianmarin4@gmail.com>
2023-07-10 09:09:19 -0600

    se creo la tarea 3

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea 3</title>
+</head>
+<body>
+
+    <h1>EVENTO DE TECLADO</h1>
+    <label for="textBox">Ingrese un texto:</label>
+    <input type="text" id="textBox">
+    <p id="output"></p>
+
+    <script src="script.js"></script> 
+    
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,8 @@
+const textBox = document.querySelector('#textBox');
+const output = document.querySelector('#output');
+textBox.addEventListener(
+    "keydown",
+    (e) => (output.textContent = `Presionaste "${e.key}".`)
+);
+
+
